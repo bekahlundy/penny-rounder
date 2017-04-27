0d016493ee48d175a4be307489deb395c224e43f
bekahlundy <lundyrebekah@gmail.com>
2017-04-27 10:44:33 -0600

    Add a few tests for TDD

--- test/moneySplitter-test.js
+++ test/moneySplitter-test.js
@@ -3,7 +3,16 @@ const { moneySplitter } = require('../lib/moneySplitter.js');
 
 describe('moneySplitter', () => {
 
-  it('exists', () => {
-    assert.deepEqual(moneySplitter(800), 800)
+  it('if total = 345.8, return [ 115.26, 115.27, 115.27 ]', () => {
+    assert.deepEqual(moneySplitter(345.8, 3), [115.26, 115.27, 115.27])
   })
+
+  it('if total = 100.00, return [ 33.34, 33.33, 33.33 ]', () => {
+    assert.deepEqual(moneySplitter(100.00, 3), [33.34, 33.33, 33.33])
+  })
+
+  it('if total = 8 and count = 4, return [2, 2, 2, 2]', () => {
+    assert.deepEqual(moneySplitter(8, 4), [2, 2, 2, 2])
+  })
+
 })
